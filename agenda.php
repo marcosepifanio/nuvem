ef582de46f75cd7ef15410d4e363067f820c71ff
Lucas Araújo <lucas.compufc@gmail.com>
2014-05-13 17:40:13 -0300

    Test Commit

--- agenda.php
+++ agenda.php
@@ -1,7 +1,14 @@
 <html>
-    <head>
-        <title>test</title>
-    </head>
-<!--AGENDA -->
-<h1>Em construção....</h1>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Zuada ...LaLALa</title>
+
+		<link rel="stylesheet" href="css/foundation.css" />
+		<script src="js/vendor/modernizr.js"></script>
+	</head>
+	<body>
+		<!--AGENDA -->
+		<h1>Em construção....</h1>
+	</body>
 </html>
