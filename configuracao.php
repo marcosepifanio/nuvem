b81d5a27853125ee9a86a9e1531e823a9cf3d3c1
Marcos <joao.marcos.ico@hotmail.com>
2014-05-14 00:06:58 -0300

    removendo configuracao.php

--- configuracao.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "quixada1.cloudapp.net";
-$user = "root";
-$pass = "J1#marcos2";
-$banco = "gerenciador_de_atividades";
-$conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
-mysql_select_db($banco) or die(mysql_error());
-?>
