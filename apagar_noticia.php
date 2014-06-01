72dc690f465c0f60fe1ed9e1a9ae903c56038988
Marcos <joao.marcos.ico@hotmail.com>
2014-06-01 15:45:28 -0300

    Apagando noticia funfando

--- apagar_noticia.php
+++ apagar_noticia.php
@@ -3,9 +3,14 @@
 	
 	if(isset($_GET['id'])){
   	$id = $_GET['id'];
+	//echo $id;
 	  //codigo apagar id
-	$query = "FROM noticias WHERE id = '$id'"; 
-	mysql_query($query);
+	$sql = "DELETE FROM noticias WHERE id = :id"; 
+	$stmt = $conexao -> prepare($sql);
+	$stmt -> bindValue(':id', $id);
+	$stmt -> execute();
+	$redirecionar = "noticias.php";
+    header("Location: $redirecionar");
 	
   } else{
   	echo "<h3>variavel nao setada</h3>";
