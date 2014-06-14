0f079a965dfd20e766ff0ac8acca9ee25a9e0d63
Lucas Araújo <lucas.compufc@gmail.com>
2014-06-13 22:44:32 -0300

    Envio de cliente SOAP

--- cliente.php
+++ cliente.php
@@ -1,3 +1,25 @@
 <?php
-    phpinfo();
+require_once "lib/nusoap.php";
+require_once 'configuracao.php';
+$client = new nusoap_client('http://localhost/nuvem/servidorSOAP.php');
+session_start();
+$autor = $_SESSION['email'];
+
+try {
+    $sqlName = 'SELECT nome from usuario where email=:email';
+    $statement = $conexao -> prepare($sqlName);
+    $statement -> bindValue(':email', $autor);
+    $statement -> execute();
+    $nome = $statement -> fetch();
+} catch (PDOException $ex) {
+    echo $ex -> getMessage();
+    die();
+}
+
+$email = $_SESSION['email'];
+
+$parametros = array('nome' => $nome['nome'], 'email' => $email);
+
+$result = $client -> call('showUser', $parametros);
+print_r($result);
 ?>
