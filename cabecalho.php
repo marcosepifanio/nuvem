28e3cc2dc81a2091f2ef03e9bdaf0c0115e8bdd1
Marcos <Marcos@192.168.0.117>
2014-06-13 22:33:59 -0300

    Adicionando Pesquisa

--- cabecalho.php
+++ cabecalho.php
@@ -1,47 +1,47 @@
 <!doctype html>
 <html class="no-js" lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Zuada ...LaLALa</title>
-		<link rel="stylesheet" href="css/foundation.css" />
-		<script src="js/vendor/modernizr.js"></script>
-	</head>
-	<body>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Zuada ...LaLALa</title>
+        <link rel="stylesheet" href="css/foundation.css" />
+        <script src="js/vendor/modernizr.js"></script>
+    </head>
+    <body>
 
-		<!--menu top-->
+        <!--menu top-->
 
-		<nav class="top-bar docs-bar hide-for-small" data-topbar="">
-			<ul class="title-area">
-				<li class="name">
-					<h1><a href="index.php">Home</a></h1>
-				</li>
-			</ul>
+        <nav class="top-bar docs-bar hide-for-small" data-topbar="">
+            <ul class="title-area">
+                <li class="name">
+                    <h1><a href="index.php">Home</a></h1>
+                </li>
+            </ul>
 
-			<section class="top-bar-section">
-				<ul class="right">
-					<li>
-						<a href="fotos_slide.php" class="">Fotos</a>
-					</li>
-					<li class="divider"></li>
-					<li>
-						<a href="atas.php" class="">Atas</a>
-					</li>
-					<li class="divider"></li>
-					<li>
-						<a href="noticias.php" class="">Noticias</a>
-					</li>
-                                        <li class="divider"></li>
-					<li>
-						<a href="pesquisa.php" class="">Pesquisa</a>
-					</li>
-					<li class="divider"></li>
-					<li>
-						<a href="sair.php" class="">Logout</a>
-					</li>
-				</ul>
-			</section>
-		</nav>
+            <section class="top-bar-section">
+                <ul class="right">
+                    <li>
+                        <a href="fotos_slide.php" class="">Fotos</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="atas.php" class="">Atas</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="noticias.php" class="">Noticias</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="pesquisa.php" class="">Pesquisa</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li>
+                        <a href="sair.php" class="">Logout</a>
+                    </li>
+                </ul>
+            </section>
+        </nav>
 
-	</body>
+    </body>
 </html>
--- pesquisa.php
+++ pesquisa.php
@@ -5,14 +5,15 @@ require_once 'session-start.php';
 
 
 <form method="post" action="pesquisa_resultado.php">
-                        <div class="row">
-                            <div class="small-6 large-centered columns">
-                                <label> 									
-                                    <textarea placeholder="Pesquisa.." name="conteudo" required=""></textarea> </label>
-                            </div>
-                        </div>
-                        <div class="small-6 large-centered columns">
-                            <input class="button success" type="submit" value="Pesquisar" />
-
-                        </div>
-                    </form>
+    <div class="row">
+        <div class="small-6 large-centered columns">
+            <label> 									
+                <textarea placeholder="Pesquisa.." name="conteudo" required=""></textarea> </label>
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-6 large-centered columns">
+            <input class="button success" type="submit" value="Pesquisar" />
+        </div>>
+    </div>
+</form>
